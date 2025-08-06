723837e93310c5d7250ee23ea5025eebf979cd25
lolodev <horaciodev1706@gmail.com>
2025-08-06 17:47:42 -0300

    Chef Claude: Header refactor

--- Header.jsx
+++ Header.jsx
@@ -1,7 +1,9 @@
+import chefClaudeLogo from "./images/chef-claude-icon.png";
+
 export default function Header() {
   return (
     <header>
-      <img src="../images/chef-claude-icon.png" alt="Chef Claude icon" />
+      <img src={chefClaudeLogo} alt="Chef Claude icon" />
       <h1>Chef Claude</h1>
     </header>
   );
