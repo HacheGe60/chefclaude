a478163366206cddb542479b5014d4b5a9d6d40b
lolodev <horaciodev1706@gmail.com>
2025-08-06 18:02:29 -0300

    Chef Claude: form

--- App.jsx
+++ App.jsx
@@ -4,9 +4,13 @@
  */
 
 import Header from "./Header.jsx";
+import Main from "./Main.jsx";
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
