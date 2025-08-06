6dd8bc47d1eab68d09f465d35d97a0e319cac656
lolodev <horaciodev1706@gmail.com>
2025-08-06 17:39:46 -0300

    Chef Claude: Header

---
+++ App.jsx
@@ -0,0 +1,12 @@
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+
+import Header from "./Header.jsx";
+
+export default function App() {
+  return (
+    <Header />
+  );
+}
