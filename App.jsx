bd2a154edbffe51e5f5e6fa4f9b30fef449a5ff8
lolodev <horaciodev1706@gmail.com>
2025-08-10 19:01:40 -0300

    get recipe from the AI chef

--- App.jsx
+++ App.jsx
@@ -3,8 +3,8 @@
  * and render it here in the App component
  */
 
-import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
 
 export default function App() {
   return (
--- components/IngredientsList.jsx
+++ components/IngredientsList.jsx
@@ -1,9 +1,7 @@
 export default function IngredientsList(props) {
-
   const ingredientsListItems = props.ingredients.map(ingredient => (
     <li key={ingredient}>{ingredient}</li>
   ));
-
   return (
     <section>
       <h2>Ingredients on hand:</h2>
@@ -13,12 +11,11 @@ export default function IngredientsList(props) {
           <h3>Ready for a recipe?</h3>
           <p>Generate a recipe from your list of ingredients.</p>
         </div>
-        <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+        <button onClick={props.getRecipe}>Get a recipe</button>
       </div>}
     </section>
   );
 }
-
 
 
 
